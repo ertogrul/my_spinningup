7136d86fac3af4c7bb00acfa2793904d04c8f66e
Michal Kordyzon <michalkordyzon@gmail.com>
2019-06-20 15:38:35 +0200

    feat: adding new start.sh file for further changes

---
+++ start copy.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+#
+# This script enables running fist few commands from spinning-up tutorial. 
+#
+
+#set the right conda environment
+command_string="source activate spinningup"
+eval "$command_string"
+output=$(eval "$command_string")
+echo "$output"
+
+# Bash Menu
+USERSCHOICE='Please enter your choice: '
+options=("Train LunarLander" "Check Lunarlander's perf" "Plot Results" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Train LunarLander")
+            echo "OK, let's train Lunarlander."
+            python -m spinup.run ppo --hid "[32,32]" --env LunarLander-v2 --exp_name installtest --gamma 0.999
+            ;;
+        "Check Lunarlander's perf")
+            echo "OK, let's check LunarLander's performance."
+            python -m spinup.run test_policy data/installtest/installtest_s0
+            ;;
+        "Plot Results")
+            echo "OK, let's see plot results."
+            python -m spinup.run plot data/installtest/installtest_s0
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo "invalid option: $REPLY";;
+    esac
+done
